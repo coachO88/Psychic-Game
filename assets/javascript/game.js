29244018ba4ff9a0179780bb50f765bda4bca0e6
coachO88 <rangel52088@gmail.com>
2019-02-16 14:10:27 -0800

    Revision

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,108 @@
 //Global Variables
 //====================================================
 //Create the Array containing NFL teams
-const Team= ['Chargers','Chiefs','Raiders','Broncos','Colts','Titans','Jaguars','Texans',
-'Patriots','Dolphins','Jets','Bills','Ravens','Bengals','Steelers','Browns','Eagles','Giants',
-'Cowboys','Redskins','Packers','Lions','Bears','Vikings','Buccaneers','Falcons','Panthers','Saints',
-'Seahawks','Rams','Niners','Cardinals'];
-//Choose Team Randomly
-var randTeam= Math.floor(Math.random() * Team.length);
-var choosenTeam= Team[randTeam];
+var Team= ['chargers','chiefs','raiders','broncos','colts','titans','jaguars','texans',
+'patriots','dolphins','jets','bills','ravens','bengals','steelers','browns','eagles','giants',
+'cowboys','redskins','packers','lions','bears','vikings','buccaneers','falcons','panthers','saints',
+'seahawks','rams','niners','cardinals'];
+//Variables
+var randTeam= "";
 var rightTeam=[];
 var wrongTeam=[];
 var underScore= [];
+var guesses=15;
+var losses=0;
+var wins= 0;
+var correctLetter= 0;
+var wrongLetter= 0;
+
+
+//Main
+function nflHangman(){
+    guesses= 15;
+
+    console.log(Team);
+    console.log("Number of Teams:", Team.length);
+
+    randTeam= Team[Math.floor(Math.random() * Team.length)]
+
+    rightTeam= randTeam.split("");
+    correctLetter= rightTeam.length;
+
+    console.log(rightTeam);
+    console.log("Number of letter:", rightTeam)
+
+
+    for (var i = 0 ; i < correctLetter; i++){
+       underScore.push('_')
+       console.log(underScore)
+    }
 
 //DOM manipulation
-var docUnderScore= document.getElementsByClassName('underscore');
-var docRightGuess= document.getElementsByClassName('rightGuess');
-var docWrongGuess= document.getElementsByClassName('wrongGuess');
-//Main
-//==================================================
-console.log(choosenTeam);
+document.getElementById("nflTeam").innerText= underScore.join(" ");
+document.getElementById("win").innerHTML= wins;
+document.getElementById("lose").innerHTML=losses;
+document.getElementById("Guesses").innerHTML=guesses;
+}
 
-//Make Underscores based on the Length of team name
-function makeUnderScore() {
-    for (var i = 0 ; i < choosenTeam.length; i++){
-       underScore.push('_');
+function scanLetter(letter){
+
+    var letterInTeam = false;
+        for(var i= 0; i < correctLetter; i++){
+            console.log(i);
+            console.log("Check Letter:", letter);
+            if (rightTeam[i] === letter){
+              letterInTeam=true;  
+            }
+            console.log("input", letter);
+            console.log("array", letter[i]);
+        }
+
+        if(letterInTeam){
+            for(var i= 0; i < correctLetter; i++){
+                if(rightTeam[i]== letter){
+                    underScore[i]= letter;
+                    console.log(underScore);
+                    console.log("check letter:", letter);
+                    document.getElementById("nflTeam").innerText= underScore.join(" ");
+                }
+            } 
+        }
+        
+        else{
+            console.log("Wrong Letter:", letter);
+            wrongTeam.push(letter);
+            document.getElementById("correctLetters").innerText= wrongTeam;
+            guesses--;
+            document.getElementById("Guesses").innerHTML= guesses
+}
+
+if (randTeam== underScore.join("")){
+    console.log(randTeam);
+    console.log(underScore.join(""));
+    document.getElementById("nflTeam").innerText= randTeam;
+    console.log(underScore)
+    var TeamName = randTeam;
+    wins++;
+} else {console.log("Not correct yet")};
+
+if (guesses==0){
+        var TeamName= randTeam;
+        alert("You Do Not Know NFL Football Team, You Lose!! The Right Team is:"+ TeamName);
+        losses++;
+        startOver();
     }
-    return underScore;
 }
-console.log(makeUnderScore());
-// docUnderScore[0].innerHTML= underScore.join(' ');
 
-//Get the letter Guess from user
-document.addEventListener('keypress', (event) =>{
-    var keyword= String.fromCharCode(event.keyCode);
-//Check if the guess is right
-for(var i=0; i < choosenTeam.length; i++){
-    if(keyword===choosenTeam[i]){
-        rightTeam.push(keyword);
-    }
+function startOver(){
+    underScore=[];
+    wrongTeam=[];
+    document.getElementById("correctLetters").innerText= wrongTeam;
+    nflHangman();
 }
-/*if(choosenTeam.indexOf(keyword) > -1){
-//If right, push to right guess array
-rightTeam.push(keyword);*/
 
-//Replace underScore with correct letter
-underScore[choosenTeam.indexOf(keyword)]= keyword;
-console.log(underScore);
-docUnderScore[0].innerHTML= underScore.join(' ');
-docRightGuess[0].innerHTML += rightTeam;
-//Checks to see if user word matches guesses
-console.log(docUnderScore);
-if (docUnderScore[0].innerHTML == choosenTeam){
-    alert('You Win');
-    
+document.onkeypress= function(event){
+    var Input= event.key;
+    console.log("Key Pressed:", Input);
+    scanLetter(Input);
 }
-else{
-    wrongTeam.push(keyword);
-    console.log(wrongTeam);
-    docWrongGuess[0].innerHTML += wrongTeam;
-}
-});
- 
-//docUnderScore["0"].innerHTML= makeUnderScore().join(' ');
-
